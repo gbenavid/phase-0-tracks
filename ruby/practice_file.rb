7563deda9b2cc6870e3b0eb04e64063ab9181ac4
Gina Benavidez <gina.commit@gmail.com>
2017-01-04 17:33:12 -0800

    Initial commit

---
+++ ruby/practice_file.rb
@@ -0,0 +1,7 @@
+# This is a practice file that will help me demonstate proper Ruby workgflow.
+puts "Hello World! My name is Ruby! :D"
+
+#Here is my second test for Ruby: Let's see if she can add these numbers for me...
+
+a = 5 + 5;
+puts a
