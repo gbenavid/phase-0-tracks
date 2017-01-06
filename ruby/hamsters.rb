2d330ea1b1b8fe4eca91b5a55a10c8ade071ae9f
Gina Benavidez <gina.commit@gmail.com>
2017-01-05 17:58:23 -0800

    Added ruby code, first attempt

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,10 +1,20 @@
 #get hamster's name 
+puts "Hamster Name:"
 #store name in var
+name = gets.chomp
 #get hamster noise level
+puts "On a scale of 1 (mute)- 10(Loud), how loud would you say your hamster is?"
 #store volume in var
+noise = gets.chomp
 #get string for hamster color
+puts "What color is your hamster?"
 #store string in var
+color = gets.chomp
 #get adoptable boolean
+puts "Is hamster adoptable? (True/ False)"
 #store boolean in var
-#get hamster age 
+adoptable = gets.chomp
+#get hamster age
+puts "How old is your hamster?"
 #store hamster age in var
+age = nil
