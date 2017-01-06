4bfb96fd27780152d0cc552869670e83112f96fe
Gina Benavidez <gina.commit@gmail.com>
2017-01-05 17:47:16 -0800

    psedocode written

---
+++ ruby/hamsters.rb
@@ -0,0 +1,10 @@
+#get hamster's name 
+#store name in var
+#get hamster noise level
+#store volume in var
+#get string for hamster color
+#store string in var
+#get adoptable boolean
+#store boolean in var
+#get hamster age 
+#store hamster age in var
