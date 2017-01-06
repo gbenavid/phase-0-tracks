64e5c64e80980454327856662b6d357c00eed439
Gina Benavidez <gina.commit@gmail.com>
2017-01-05 20:20:22 -0800

    Deleted pseudecode and added var's with corresponding string promts.

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,5 +1,12 @@
-#what's your name
-#how old are you?
-#what year were you born?
-#Our company cafeteria serves garlic bread. Should we order some for you?
-#Would you like to enroll in the company’s health insurance? 
+#RELEASE 1: Interview with the Vampire
+
+puts "what's your name"
+name = gets.chomp
+puts "how old are you?"
+age = gets.chomp
+puts "what year were you born?"
+dob = gets.chomp
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+likes_garlic = gets.chomp
+puts "Would you like to enroll in the company’s health insurance?"
+needs_healthcare = gets.chomp
