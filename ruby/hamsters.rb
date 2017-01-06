c9e24bea153cb6d95598c1083f4857a47fe89804
Gina Benavidez <gina.commit@gmail.com>
2017-01-06 11:20:10 -0800

    Debugging. Added age value to adjust for nil

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -5,7 +5,7 @@ name = gets.chomp
 #get hamster noise level
 puts "On a scale of 1 (mute)- 10(Loud), how loud would you say your hamster is?"
 #store volume in var
-noise = gets.to_i
+noise = gets.chomp.to_i
 #get string for hamster color
 puts "What color is your hamster?"
 #store string in var
@@ -18,6 +18,10 @@ adoptable = gets.chomp
 puts "How old is your hamster?"
 #store hamster age in var
 age = gets.chomp
+if age == ''
+	age = nil
+else age = age.to_i
+end
 puts "Hamster data:
 Name: #{name}
 Age: #{age}
