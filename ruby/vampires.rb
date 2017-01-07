e5393c0734cdb50cef793f2ca4e6e94601cde246
Gina Benavidez <gina.commit@gmail.com>
2017-01-07 10:18:20 -0800

    added some practice elsif statements before I started working on release 2w

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,30 +1,40 @@
 #RELEASE 1: Interview with the Vampire
-puts "what's your name"
-name = gets.chomp
+# puts "what's your name"
+# name = gets.chomp
 
 
-puts "how old are you?"
-age = get.chomp.to_i
+# puts "how old are you?"
+# age = get.chomp.to_i
 
-puts "what year were you born?"
-dob = gets.chomp.to_i
-# validAge = 2017 - dob == age
+# puts "what year were you born?"
+# dob = gets.chomp.to_i
+# # validAge = 2017 - dob == age
 
-puts "Our company cafeteria serves garlic bread. Should we order some for you?"
-garlic = gets.chomp
+# puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+# garlic = gets.chomp
 
-puts "Would you like to enroll in the company’s health insurance?"
-healthcare = gets.chomp
+# puts "Would you like to enroll in the company’s health insurance?"
+# healthcare = gets.chomp
 
-puts "
-(#{name}, #{age}, #{dob}, #{garlic}, #{healthcare})
-"
+# puts "
+# (#{name}, #{age}, #{dob}, #{garlic}, #{healthcare})
+# "
 
 #RELEASE 2: Detection Logic
+
+
 wolves_like_sunshine = true
 wolves_like_garlic = true
 vampires_like_sunshine = false
 vampires_like_garlic = false
+
+#Example elsif statment:
+if (vampires_like_garlic)
+	puts "Not a vampire"
+elsif (wolves_like_sunshine)
+	puts "Someone enjoys garlic"
+end
+
 
 #def defoA_Vampire = 
 	# define a method in Ruby.
@@ -39,6 +49,15 @@ vampires_like_garlic = false
 
 # go in order as prompted
 #check out time class
+
+
+
+
+
+
+
+
+
 
 
 
