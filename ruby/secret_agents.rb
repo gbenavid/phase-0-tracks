f7dca970e94a7c3a0d087dd37271fe72dc4b47da
Gina Benavidez <gina.commit@gmail.com>
2017-01-08 22:21:26 -0800

    Added intro comments to describe to the reader what I will be doing in the program

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,51 +1,49 @@
-=begin
-pseudocode the encrypt and decrypt method
-=end
 
 #encrypt
 
-#define a method called encrypt that will advance evert letter of a string one letter forward
+#define a method called encrypt that will advance every letter of a string one letter forward
 def encrypt(password)
   #set index inside method - scope
   index = 0
-   
-#find out how many times we have to loop through the word
 #loop through it while the index is < than the length of the word
-  while index < password.length
-   
-  #shift the letter by one and update password
-  password[index] = password[index].next
-   
-  #update index
-  index += 1
+  if password[index] == "z"
+        password[index] = "a"
+        index += 1
+    end
+  while index < password.length    
+    #shift the letter by one and update password
+    password[index] = password[index].next
+
+    #update index
+    index += 1
   end
   #close loop and print password 
   p password
 end
 
+# def decrypt(password)
+#   #store alphabet in a string so that we can access it.
+#   alphabet = "abcdefghijklmnopqrstuvwxyz"
+  
+#   #loop through the alphabet and match password and alphabet against each other.
+#   #nested loop
 
-#decrypt
-
-#define decrypt method that will go backward one letter of the alphabet
-def decrypt(password)
-   
-  #set index to zero
-  index = 0
-   
-  #store alphabet in a string so that we can access it.
-  alphabet = "abcdefghijklmnopqrstuvwxyz"
-   
-#loop through it while index < the length of the word
-  while index < password.length
-
-#shift the letter by one and update password
-  #check what letter it is
-   
-
-  password[index] = 
-
-#update index
-
-#consider edge case. conditional? 
-
-#close loop and print password
+#   #set index to zero
+#   index = 0 
+  
+#   #decryption_index = nil
+  
+#   while index < password.length 
+#     counter_alpha = 0
+    
+#     until password[index] == alphabet[counter_alpha]
+#     counter_alpha += 1
+#     #store counter alpha into another variable
+    
+#     end 
+#          password[index] = alphabet[counter_alpha - 1]
+    
+#   index += 1 
+#   end
+#   p password
+# end
--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,146 +1,32 @@
-puts "How many employees are we processing today?"
-people_processed = gets.chomp.to_i 
+# RELEASE 1: Interview with the Vampire
+# Here we want to collect intelligence from our candidate. We will make use of this information latter on when we determine wheather s/he is a vampire or not. 
+puts "what's your name"
+input = gets.chomp
 
-people_processed.times do 
-  puts "What's your name?"
-  interviewees_name = gets.chomp
-    
-  puts "How old are you?"
-  employee_age = gets.chomp.to_i 
-  
-  #puts "(Birth year: ~#{Time.new.year - employee_age.to_i})"
-  
-  likes_garlic = false
-    until likes_garlic
-    puts "Our company cafeteria serves garlic bread. Do you like garlic (true or false)?"
-    garlic_answer = gets.chomp 
-      else
-       puts "I'm sorry you must respond with 'true' or 'false'"
-      end 
-    end 
-  
-  insurance_answer = false
-    until insurance_answer
-      puts "Would you like to enroll in the company's health insurance? y/n"
-      insurance_yn = gets.chomp 
-        if insurance_yn == "y"
-          insurance_answer = true
-        elsif insurance_yn == "n"
-          insurance_answer = true
-        else 
-          puts "y or n response only."
-        end 
-    end 
-  
-  puts "Summary: #{interviewees_name}, #{employee_age} years old. Wants health insurance: #{insurance_yn}, wants garlic bread: #{garlic_yn}."
-  
+puts "how old are you?"
+age = gets.chomp
+puts "what year were you born?"
+dob = gets.chomp
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+garlic = gets.chomp
+puts "Would you like to enroll in the company’s health insurance?"
+healthcare = gets.chomp
 
-    if (employee_age.to_i >=18 && employee_age.to_i <=85) && (garlic_yn == "y" || insurance_yn == "y") && !(interviewees_name == "Drake Cula" || interviewees_name == "Tu Fang")
-      puts "PROBABLY NOT A VAMPIRE"
-    elsif !(employee_age.to_i >=18 && employee_age.to_i <=85) && (garlic_yn == "y" || insurance_yn == "y") && !(interviewees_name == "Drake Cula" || interviewees_name == "Tu Fang")
-      puts "PROBABLY A VAMPIRE"
-    elsif !(employee_age.to_i >=18 && employee_age.to_i <=85) && (garlic_yn == "n" && insurance_yn == "n") && !(interviewees_name == "Drake Cula" || interviewees_name == "Tu Fang")
-      puts "ALMOST CERTAINLY A VAMPIRE"
-    elsif (interviewees_name == "Drake Cula" || interviewees_name == "Tu Fang")
-      puts "DEFINITELY A VAMPIRE"
-    else 
-      puts "RESULTS INCONCLUSIVE"
-    end 
-  
-  allergy_yn = false 
-  until allergy_yn 
-  puts "Please list all alergies. Type 'done' when finsihed."
-  allergy = gets.chomp 
-      if allergy == "sunshine"
-      puts "PROBABLY A VAMPIRE"
-      allergy_yn = true 
-      elsif allergy == "done"
-      allergy_yn = true 
-      end 
-  end 
-  
-end 
-
-puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
-
-#RELEASE 1: Interview with the Vampire
-
+puts "
+(#{name}, #{age}, #{dob}, #{garlic}, #{healthcare})
+"
 #RELEASE 2: Detection Logic
+wolves_like_sunshine = true
+wolves_like_garlic = true
+vampires_like_sunshine = false
+vampires_like_garlic = false
 
-# wolves_like_sunshine = true
-# wolves_like_garlic = true
-# vampires_like_sunshine = false
-# vampires_like_garlic = false
-
-#Example elsif statment:
-# if (vampires_like_garlic)
-# 	puts "Not a vampire"
-# elsif (wolves_like_sunshine)
-# 	puts "Someone enjoys garlic"
-# end
-
-# IRB Practice
-# In terminal run:
-# wolves_like_garlic && wolves_like_sunshine --->> true
-# wolves_like_garlic || vampires_like_garlic --->> true
-# wolves_like_garlic && vampires_like_garlic ---false
-# wolves_like_garlic && (vampires_like_sunshine || vampires_like_garlic) ---> false
-# (wolves_like_garlic && vampires_like_garlic) || wolves_like_sunshine ---> true
-# vampires_like_garlic ---> false
-# !vampires_like_garlic ---> true
-# !(wolves_like_sunshine && wolves_like_garlic) ---> false	
-# p "How many employees will we be processing today?"
-# interviewees = gets.to_i
-# p "What's your name?"
-# title = gets.chomp
-
-# puts "What's your age?"
-# age = gets.to_i
-
-# puts "What year were you born?"
-# dob = Time.new.year - gets.to_i
-# validAge = age == dob 
-
-# puts "I enjoy eating garlic bread. (true or false)"
-# likes_garlic = gets.chomp
-
-# puts "I am interested in signing up for health at Werewolf Inc. (true/ false)"
-# healthcare = gets.chomp
-
-# tuFang = "Tu Fang"
-# drakeCula = "Drake Cula"
-
-# inteligence = ["#{title}", "#{age}", "#{likes_garlic}", "#{healthcare}"]
-# # p "#{title} is #{age} years old and has a #{likes_garlic} love for garlic. This person's interest in our healthcare system seems to be #{healthcare} and therefore, I suspect that..."
-
-# def vampire_detector ("title", validAge, likes_garlic, healthcare)
-# 	if (title = ("Tu Fang" || "Drake Cula"))
-# 		p "Definitely a vampire"
-# 	elsif (validAge && (likes_garlic || healthcare))
-# 		p "Probably not a vampire."
-# 	elsif !(validAge && likes_garlic && healthcare)
-# 		p "Almost certainly a vampire."
-# 	elsif !(validAge && (likes_garlic || healthcare))
-# 		p "Probably a vampire"
-# 	else p "Results inconclusive"
-# 	end
-# end
-# vampire_detector (title, validAge, likes_garlic, healthcare)
-#("#{title}", "#{validAge}", "#{likes_garlic}", "#{healthcare}")
-# i = 1
-# while i < interviewees do 
-# 	logic
-# 	i += 1
-# end
-
-# RELEASE 3 : Process Multiple Employees
-
-# go in order as prompted
-
-# Begin the surey process of the first employee.
-# Print the result
-# Then begin the processing for the next employee
-# A while loop stops when a conditional is false
-# An until loop stops when it's conditional is true
-# employees_interviewed = 0
-# p logic 
+#def defoA_Vampire = 
+  # define a method in Ruby.
+  # create a var to hold result
+  # if name === 'tu fang' || 'Drake Cula' results = "definitley a vampire"
+  # if (age = true && (garlic bread = true || healthcare = true)) result = "probably not a vampire"
+  # if (age = false && (garlic == false || healthcare = false)) result = probably a vampire
+  # if (age = false && garlic = false && healthcare = false) result = "Almost certainly a vampire"
+  # Otherwise result = "Results Inconclusive"
+  #return result. 
