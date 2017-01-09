f5a3709d6156a5ec248646ce375f74b66bc7af75
Gina Benavidez <gina.commit@gmail.com>
2017-01-08 22:44:47 -0800

    Added notes on conditionals in 'Notes to self' section

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -5,21 +5,38 @@ input = gets.chomp
 
 puts "how old are you?"
 age = gets.chomp
+
 puts "what year were you born?"
 dob = gets.chomp
+
 puts "Our company cafeteria serves garlic bread. Should we order some for you?"
 garlic = gets.chomp
+
 puts "Would you like to enroll in the company’s health insurance?"
 healthcare = gets.chomp
 
 puts "
 (#{name}, #{age}, #{dob}, #{garlic}, #{healthcare})
 "
+
+
+
+
+
+
+
 #RELEASE 2: Detection Logic
-wolves_like_sunshine = true
-wolves_like_garlic = true
-vampires_like_sunshine = false
-vampires_like_garlic = false
+
+# ------NOTES TO SELF
+
+# true && true --->> true
+# true || false --->> true
+# true && false ---false
+# true && (false || false) ---> false
+# (true && false) || true ---> true
+# false ---> false
+# !false ---> true
+# !(true && true) ---> false  
 
 #def defoA_Vampire = 
   # define a method in Ruby.
