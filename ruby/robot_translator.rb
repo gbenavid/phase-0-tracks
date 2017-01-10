4ccb7026eb6a0b07ec006c9b6e34544455f8e108
Gina Benavidez <gina.commit@gmail.com>
2017-01-09 20:07:10 -0800

    Added to conditional logic in translated_character method to handle characters that are in the begining of the alphabet and are also capitolized.

--- ruby/robot_translator.rb
+++ ruby/robot_translator.rb
@@ -1,15 +1,18 @@
  # ROBOT TRANSLATOR
+
  # If a letter is capitalized & its in the first half of the alphabet, it becomes "Bloop."
  # Otherwise, it a letter is capitalized or it's the letter "e", it becomes "buzz".
- # Otherwise, it becomes "bleep".
+
 def translated_character(character)
 	alphabet = "abcdefghijklmnopqrstuvwxyz"
 	halfway = alphabet.length/2
 	is_capitolized = (character.upcase == character)
 	# Now let's start in on some conditional logic!
 	if !alphabet.index(character.downcase)
-	# if the lowercase version doesn't apear
+	# if the character is not in the alphabet
 		"~BOING"
+	elsif is_capitolized && (alphabet.index(character.downcase) +1) <= halfway	
+		"~Bloop."
 	else
 		"~BLEEP"
 	end
@@ -19,9 +22,9 @@ def translate(string)
 	character_index = 0
 	translated_phrase = "" 
 	while character_index < string.length 
-		translated_phrase << translated_character(string[character_index]) # Updates translated phase with one translated character at a time
+		translated_phrase << translated_character(string[character_index])
 		character_index += 1
 	end
 	translated_phrase
 end
-puts translate("UL 1!")
+puts translate("!AaZ") #>> ~BOING.~BLOOP~BLEEP~BLEEP
