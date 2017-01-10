4f7479fb1856638b95628ba6195aa6d8b009e76b
Gina Benavidez <gina.commit@gmail.com>
2017-01-09 19:33:27 -0800

    changed output strings to be more legible by adding ~tildas, also added the last conditional outcome for all false statements in translated_character to return ~BLEEP.

--- ruby/robot_translator.rb
+++ ruby/robot_translator.rb
@@ -1,21 +1,18 @@
  # ROBOT TRANSLATOR
  # If a letter is capitalized & its in the first half of the alphabet, it becomes "Bloop."
  # Otherwise, it a letter is capitalized or it's the letter "e", it becomes "buzz".
- # If it's not a lletter at all it becomes "boing".
  # Otherwise, it becomes "bleep".
 def translated_character(character)
-	# Let's set some variables to give our program the brainpower it needs to excecute our conditional logic.
 	alphabet = "abcdefghijklmnopqrstuvwxyz"
 	halfway = alphabet.length/2
 	is_capitolized = (character.upcase == character)
 	# Now let's start in on some conditional logic!
 	if !alphabet.index(character.downcase)
 	# if the lowercase version doesn't apear
-		"boing"
+		"~BOING"
 	else
-		character
+		"~BLEEP"
 	end
-	# character ---i had to remove this line of code before moving on because 
 end
 
 def translate(string)
@@ -27,4 +24,4 @@ def translate(string)
 	end
 	translated_phrase
 end
-puts translate("testing!   ")
+puts translate("UL 1!")
