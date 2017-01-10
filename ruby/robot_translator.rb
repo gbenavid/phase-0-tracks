80e9a7fb00b4b9def061d775e0a4521b665b6422
Gina Benavidez <gina.commit@gmail.com>
2017-01-09 19:07:06 -0800

    Working on translated_character logic. setting up variables

--- ruby/robot_translator.rb
+++ ruby/robot_translator.rb
@@ -4,16 +4,24 @@
  # If it's not a lletter at all it becomes "boing".
  # Otherwise, it becomes "bleep".
 def translated_character(character)
-	character = "T" #Test condition to check workflow
+	# if not a letter
+	
+	# find out if letter is capitolized
+	alphabet = "abcdefghijklmnopqrstuvwxyz"
+	halfway = alphabet.length/2
+	is_capitolized = (character.upcase == character)
+	# beginingof_alphabet ()
+	# p "Bloop"
+else #pushes character into another medthod we haven't yet defined
 end
 
 def translate(string)
 	character_index = 0
-	translated_phrase = "" # Here we're creatingan empty string that will hold the data we push into it. In this case we want to translate each character into a corresponding string.
+	translated_phrase = "" 
 	while character_index < string.length 
 		translated_phrase << translated_character(string[character_index]) # Updates translated phase with one translated character at a time
 		character_index += 1
 	end
-	translated_phrase #Returns our translated phrase
+	translated_phrase
 end
 puts translate("testing")
