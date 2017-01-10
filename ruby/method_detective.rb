356b8dc793bdabe5239599002dfabb2e512c0364
Gina Benavidez <gina.commit@gmail.com>
2017-01-10 00:39:06 -0800

    Progressed in finding ruby string methods. Added .concat, and .ord

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -3,26 +3,22 @@
 
 # When there's more than one suspect who could have committed the crime, add additional calls to prove it.
 
-"iNvEsTiGaTiOn".swapcase
-# => “InVeStIgAtIoN”
+"iNvEsTiGaTiOn".swapcase # => “InVeStIgAtIoN”
 
 # "zom".tr('o', 'oo')
 # => “zoom”
 
-"enhance".center(20)
-# => "    enhance    "
+"enhance".center(20) # => "    enhance    "
 
-"Stop! You’re under arrest!".upcase
-# => "STOP! YOU’RE UNDER ARREST!"
+"Stop! You’re under arrest!".upcase # => "STOP! YOU’RE UNDER ARREST!"
 
-# "the usual".<???>
-#=> "the usual suspects"
+"the usual".concat(" suspects")
+"the usual" << " suspects" #=> "the usual suspects"
 
 # " suspects".<???>
 # => "the usual suspects"
 
-"The case of the disappearing last letter".chop
-# => "The case of the disappearing last lette"
+"The case of the disappearing last letter".chop # => "The case of the disappearing last lette"
 
 # "The mystery of the missing first letter".<???>
 # => "he mystery of the missing first letter"
@@ -30,9 +26,9 @@
 # "Elementary,    my   dear        Watson!".<???>
 # => "Elementary, my dear Watson!"
 
-# "z".<???>
+"z".ord
 # => 122 
-# (What is the significance of the number 122 in relation to the character z?)
+# 122 is the integer ordinal of z.
 
 "How many times does the letter 'a' appear in this string?".count "a"
 # => 4
