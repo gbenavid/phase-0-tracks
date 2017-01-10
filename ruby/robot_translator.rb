aef949710e89acd793f8f9eafdba0ddab44c4228
Gina Benavidez <gina.commit@gmail.com>
2017-01-09 18:45:48 -0800

    Changed a few names in methods and variables to enhance readability. Added a translated_chracter methods that turns all characters into capitolized T's

--- ruby/robot_translator.rb
+++ ruby/robot_translator.rb
@@ -3,12 +3,17 @@
  # Otherwise, it a letter is capitalized or it's the letter "e", it becomes "buzz".
  # If it's not a lletter at all it becomes "boing".
  # Otherwise, it becomes "bleep".
-def translator(phrase)
-	character_index = 0 # setting our counter
-	while character_index < phrase.length # Telling the loop to end when the character index is less than the phrase length (when the string finishes)
-		puts phrase[character_index] # Prints our chracter index 
-		character_index += 1 # Index increments by 1 after our condition is preformed so we're not stuck in an infinite loop
-	end
+def translated_character(character)
+	character = "T" #Test condition to check workflow
 end
-# Before we move on let's test our foundation. So far we've created a loop, let's see if that loop can print the letters in our test phrase.
-translator("testing")
+
+def translate(string)
+	character_index = 0
+	translated_phrase = "" # Here we're creatingan empty string that will hold the data we push into it. In this case we want to translate each character into a corresponding string.
+	while character_index < string.length 
+		translated_phrase << translated_character(string[character_index]) # Updates translated phase with one translated character at a time
+		character_index += 1
+	end
+	translated_phrase #Returns our translated phrase
+end
+puts translate("testing")
