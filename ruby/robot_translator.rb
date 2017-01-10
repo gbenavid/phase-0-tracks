80191c77bc000d55d9c7dc68cf8dc663b6897ae8
Gina Benavidez <gina.commit@gmail.com>
2017-01-09 18:15:06 -0800

    Fixed bug in loop. Infinite loop was removed after the =+ on line 10 was changed to =+

--- ruby/robot_translator.rb
+++ ruby/robot_translator.rb
@@ -4,10 +4,11 @@
  # If it's not a lletter at all it becomes "boing".
  # Otherwise, it becomes "bleep".
 def translator(phrase)
-	# Because we're going to be referencing an character index for the string we'll need to set a loop.
-	character_index = 0 # Setting the counter for the loop
-	while character_index < phrase.length
-		p phrase[character_index] # Prints our chracter index
-		character_index =+ 1 # Updates are index after our puts is preformed so we're not stuck in an infinite loop
+	character_index = 0 # setting our counter
+	while character_index < phrase.length # Telling the loop to end when the character index is less than the phrase length (when the string finishes)
+		puts phrase[character_index] # Prints our chracter index 
+		character_index += 1 # Index increments by 1 after our condition is preformed so we're not stuck in an infinite loop
 	end
 end
+# Before we move on let's test our foundation. So far we've created a loop, let's see if that loop can print the letters in our test phrase.
+translator("testing")
