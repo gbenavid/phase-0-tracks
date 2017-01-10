c4cf7b27eefcb57039289639fa3b60f26d34e753
Gina Benavidez <gina.commit@gmail.com>
2017-01-10 12:56:42 -0800

    Pseudocoded the application required feilds

---
+++ ruby/interior_design_application.rb
@@ -0,0 +1,17 @@
+# Process the interior design application
+# Name
+Address:
+Email:
+Phone:
+
+Fave Shade of bule:
+Wallpaper preferences (Check any that apply):
+* Paisley
+* Cheverons
+* Photorealistic woodsy scenes (with or without squirrels)
+* Abstract woodsy scenes (no squirrels) 
+
+Obmbre is:
+* Fierce
+* So last season
+* Practically medieval in its appallling irrelevance
