ef324c3b4bcae57d2b9f17617bf5daa291bf1d42
Gina Benavidez <gina.commit@gmail.com>
2017-01-10 16:37:55 -0800

    modified driver code to application key value pairs.

--- ruby/interior_design_application.rb
+++ ruby/interior_design_application.rb
@@ -1,17 +1,29 @@
 # Process the interior design application
-# Name
-Address:
-Email:
-Phone:
+# Do we care if the data is readable 
+application = {
+	name: "Gina Ghram",
 
-Fave Shade of bule:
-Wallpaper preferences (Check any that apply):
-* Paisley
-* Cheverons
-* Photorealistic woodsy scenes (with or without squirrels)
-* Abstract woodsy scenes (no squirrels) 
+	address: "1400 Play St. Apt 12 Boston",
 
-Obmbre is:
-* Fierce
-* So last season
-* Practically medieval in its appallling irrelevance
+	email: "gina-g@gmail.com",
+
+	phone: "123.234.3456",
+
+	fav_bule_hues: "periwinkle, Lemongrass, navy",
+
+	wallpaper_preferences: [
+		paisley: true, 
+		cheverons: false, 
+		photorealistic_1: false, 
+		photorealistic_2: true], 
+	obmbre_is: [
+	fierce: false,
+	last_season: true,
+	medieval: false]
+}
+p application
+# Add a value that let's us know that we have/n't hirred this person.
+application["hirred"] = true
+application[:name] = "Gina Benavidez"
+p application
+p application[:wallpaper_preferences]
