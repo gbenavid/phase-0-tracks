596295ccd2bcc78cefb57fce1ee7f6bb7e78e8c8
Gina Benavidez <gina.commit@gmail.com>
2017-01-10 23:40:46 -0800

    Added more pseudocode to set up more detailed information about the expectations of my program, like user input and output values.

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,3 +1,22 @@
-# This program should allow our interior designer to enter the following details about their client: name, age, number of children, decor theme, and so on.
+# This program should allow our interior designer to enter the following details about their 
+# client: 
+# name, 
+# age, 
+# number of children, and
+# decor theme
 
-# Note: Keys should be symbols
+# Note: Use symbols
+
+
+
+# ----- The Program -----
+# Promt the user for all of this information.
+
+# Convert any user input to the appropriate data type.
+
+# Print the hash back out to the screen when the designer has answered all of the questions.
+
+# Give the user the opportunity to update a key, because sometimes users make mistakes! 
+# If the designer says "none", skip it. But if the designer enters "decor_theme" (for example), your program should ask for a new value and update the :decor_theme key. (Hint: Strings have methods that will turn them into symbols, which would be quite handy here.) You can assume the user will correctly input a key that exists in your hash -- no need to handle user errors.
+
+# Print the latest version of the hash, and exit the program.
