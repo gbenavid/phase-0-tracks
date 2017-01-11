634d5b64d092cec7d817815989444ad392128ea1
Gina Benavidez <gina.commit@gmail.com>
2017-01-11 09:49:01 -0800

    Finished program. Added conditional logic that updates a hash value.

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,3 +1,5 @@
+
+# ----- The Program -----
 # Note: Use symbols
 p "Please type carefully when entering the following information about your client."
 # Promt the user for all of this information.
@@ -6,38 +8,36 @@ p "Please type carefully when entering the following information about your clie
 	# age, 
 	# number of children, and
 	# decor theme
-p "Name:"
+p "Name of client:"
 client_name = gets.chomp
 p "Age:"
 age = gets.chomp
-p "How many kids does your client have?"
+p "How many kids does your client have? (enter 0 if none)"
 # Convert any user input to the appropriate data type. 
-kids = gets.chomp
+kids = gets.chomp.to_i
 p "The decor is a custom theme (true/false)?"
 custom_theme = gets.chomp
 
+# -----client data in an array with key values-----
 client_data = {
 	:client_name => client_name,
 	:age => age,
-	:kids => has_kids,
+	:kids => kids,
 	:custom_theme => custom_theme}
 # Print the hash back out to the screen when the designer has answered all of the questions.
 p client_data
-p "Does all that information look right to you?"
-p "If all the information is true, type 'true', otherwise type 'false'"
+p "^ ^ ^ ^ ^ ^ Does all that information look right to you?"
+p "If all the information is correct type 'none', otherwise press enter"
 response = gets.chomp
-if !response
-	p "Okay, what would you like to correct?"
-	
+if response == 'none'
+	p "Okay great! Here are your final results:"
+elsif			
+	p "Okay, what feild needs a correction (e.g client_name)?"
+	correct_sym = gets.chomp.to_sym
+	p "What would you like it to say instead?"
+	correct_value = gets.chomp
+	#update corresonding key
+	client_data[correct_sym] = correct_value
 end
-
-# ----- The Program -----
-
-def clients_preferences
-
-
-# Give the user the opportunity to update a key, because sometimes users make mistakes! 
-# If the designer says "none", skip it. But if the designer enters "decor_theme" (for example), your program should ask for a new value and update the :decor_theme key. (Hint: Strings have methods that will turn them into symbols, which would be quite handy here.) You can assume the user will correctly input a key that exists in your hash -- no need to handle user errors.
-end
-p clients_preferences
 # Print the latest version of the hash, and exit the program.
+p client_data
