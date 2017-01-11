119e2b0075d7e8f8c5a798efeb6960acdcbfc975
Gina Benavidez <gina.commit@gmail.com>
2017-01-11 15:57:18 -0800

    Pseudocoding: Working on the reversal of the first and last name

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,17 @@
+# Pseudocoding 
+# Mission: Help out our friends at DBC create fake names!
+# Input => "First Last"
+# Ask the user for their name
+# p "Please enter your real name and we will protect your identity by generating a new one for you:"
+# name = gets.chomp
+
+
+# def secret_name (name)
+	# Swapping the first and last name.
+	# .split
+	#.reverse
+	# Changing all of the vowels (a, e, i, o, or u) to the next vowel in 'aeiou', 
+	# and all of the consonants to the next consonant in the alphabet.
+# end
+# secret_name(name)
+# Output => "Letu Gostu"
