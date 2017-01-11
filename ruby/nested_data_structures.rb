4191902eac6a2f3435420b3b81886f6a71512225
Gina Benavidez <gina.commit@gmail.com>
2017-01-11 12:50:29 -0800

    Populating object 'hillarious_books' with string values, and key value pairs

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,29 @@
+hillarious_books = {
+	klien: {
+		author_full_name: "Jessie Klien"
+		book_title: "You'll grow out of it"
+		top_chapters: [
+			"Poodle vs. Wolf", 
+			"Tom Man", 
+			"Dale"]
+		pages: 291
+	}
+	kalling: {
+		author_full_name: "Mindy Kalling"
+		book_title: "Is everyone hanging out without me (and other concerns)"
+		top_chapters: [
+			"Chubby for Life",
+			"When your not skinny this is what people want you to wear"] 
+	}
+	Amorusso:{
+		author_full_name: "Sophia Amoruso"
+		book_title: "#GIRLBOSS"
+		top_chapters: [
+			"Shoplifting (and Hitchhiking) Saved My Life",
+			"On Hirring, Staying Employeed, and Firing."
+		]
+
+	}
+	# Mindy Kalling - Is everyone hanging out without me (and other concerns)
+	# Sophia Amorusso - Girl Boss
+}
