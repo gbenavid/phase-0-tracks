07a791277de20a66346ccd3ffdc7f80ba0d9a792
Gina Benavidez <gina.commit@gmail.com>
2017-01-12 13:19:18 -0800

    Nil checking for index algorithim

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,25 +1,22 @@
 # Array: Take a number and return it's index and return nil if it's non existant in the array
 arr = [42, 89, 23, 2]
-# array_length = array.length
 def search_arr (arr, integer)		
 	new_arr = [] 
 	arr.each do |number|
-		new_arr << number
+	new_arr << number
 		if (number == integer)
-			p new_arr.length - 1
-			# p arr.length # PRINT THE INDEX
+			return new_arr.length - 1
+		elsif (new_arr.length == arr.length)
+			return nil
 		end
 	end
-	# If the parameter is equal to any of the indecies 
-	# return the number.
 end
-p arr
-p search_arr(arr, 2)
+puts search_arr(arr, 2123)
 # Input => One number
-# def fib (number)
+def fib (number)
 	# Take the number and generate an array that is the length of the number.
 
-# end
+end
 # Output => an array of numbers
 
 
