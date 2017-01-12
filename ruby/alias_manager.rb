95cc775283448c9cf8a30abde5367f5ebb30038a
Gina Benavidez <gina.commit@gmail.com>
2017-01-11 22:56:12 -0800

    Added the #next feature to the program. *NOTE* THIS IS THE LAST ATTEMPT TO COMPLEATE THIS CHALLENGE USING #map!

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -4,21 +4,16 @@
 p "Please enter your (real) first name and we will protect your identity by generating a new one for you:"
 name = gets.chomp
 
-# Input => "Gina Benavidez"
 def secret_name (name)
 	name = name.downcase.split(' ').reverse
-	# ["benavidez", "gina"]
 	# If 'aeiou' => 'eioua' respectivley.
 	# ---letter---
-
+	name = name.join(' ').chars
 	# Create a loop for vowels >>> put it into each
 	# For each character compare it to 'a'e'i'o'u'
-	name.map { |letter| (
-		letter == 'a' ? letter == "e" 
-		: letter.next
-		)}
+	name.map! { |letter| (letter == "a" ? letter = "e" : letter.next)}
+
 	# match the string to the letters in 'aeio'
-	# if the condition is true, return the next vowel (.next)
 	# and all of the consonants to the next consonant in the alphabet.
 end
 
