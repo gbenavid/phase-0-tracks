45e3e2f59dcf59691a668e324126c3e5e0bc6e96
Gina Benavidez <gina.commit@gmail.com>
2017-01-12 13:08:24 -0800

    Finished algorithm that mimics .index(). Needs nil checking

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,19 +1,20 @@
 # Array: Take a number and return it's index and return nil if it's non existant in the array
 arr = [42, 89, 23, 2]
 # array_length = array.length
-def search_arr (arr, integer)
+def search_arr (arr, integer)		
+	new_arr = [] 
 	arr.each do |number|
-		if number == integer
-			p true # p arr[i] print the index that this intger is located at
-		else 
-			p false # otherwise add one to the conter until loop ends
+		new_arr << number
+		if (number == integer)
+			p new_arr.length - 1
+			# p arr.length # PRINT THE INDEX
 		end
 	end
 	# If the parameter is equal to any of the indecies 
 	# return the number.
 end
 p arr
-p search_arr(arr, 42)
+p search_arr(arr, 2)
 # Input => One number
 # def fib (number)
 	# Take the number and generate an array that is the length of the number.
