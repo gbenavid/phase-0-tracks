7f0000173b74881e2f2c126eae026eb1cf22e0f5
Gina Benavidez <gina.commit@gmail.com>
2017-01-11 17:14:22 -0800

    added a loop using .each that loops each letter in the users first name. Returns an unrelated statment to the console.

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,18 +1,44 @@
 # Pseudocoding 
 # Mission: Help out our friends at DBC create fake names!
 
-# Ask the user for their name
-p "Please enter your real name and we will protect your identity by generating a new one for you:"
-name = gets.chomp
+# p "Please enter your (real) first name and we will protect your identity by generating a new one for you:"
+# name = gets.chomp
 
-# Input => "First Last"
+# Input => "First"
 def secret_name (name)
-	# Swapping the first and last name.
-	# .split
-	#.reverse
-	name.split.reverse
-	# Changing all of the vowels (a, e, i, o, or u) to the next vowel in 'aeiou', 
+	name = name.downcase.split("")
+	# ===> 'gina'
+	
+	# If the string 'name' has any characters that match 'aeiou' we will turn those into the next vowel.
+	# ----
+	
+	# split the entire name into a string of downcase characters
+	# name.split("")
+	# ['g', 'i', 'n', 'a']
+	
+	# let the computer know what a vowel is
+	vowels = ['a', 'e', 'i', 'o', 'u']
+	# For each character compare it to 'a'e'i'o'u'
+	name.each do |letter|
+		# print the next vowel respectivly 
+		p "I love you #{letter}"
+		# 'I love you b'
+		# 'I love you e'
+		# 'I love you n'...etc.
+	end
+	# match the string to the letters in 'aeio'
+	# if the condition is true, return the next vowel (.next)
 	# and all of the consonants to the next consonant in the alphabet.
 end
-p secret_name(name)
+p secret_name('GINA')
 # Output => "Letu Gostu"
+
+
+
+
+
+
+
+
+
+
