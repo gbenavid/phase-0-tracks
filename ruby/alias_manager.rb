14b3af56186ca2c09f7d9ace97bb0adca1503d6c
Gina Benavidez <gina.commit@gmail.com>
2017-01-12 00:00:41 -0800

    created 3 definitions to 1. reverse the string, 2. move to the next character and 3. set a condition. Still working on condition

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -2,20 +2,33 @@
 # Mission: Help out our friends at DBC create fake names!
 
 p "Please enter your (real) first name and we will protect your identity by generating a new one for you:"
-name = gets.chomp
-
-def secret_name (name)
-	name = name.downcase.split(' ').reverse
-	# If 'aeiou' => 'eioua' respectivley.
-	# ---letter---
-	name = name.join(' ').chars
-	# Create a loop for vowels >>> put it into each
-	# For each character compare it to 'a'e'i'o'u'
-	name.map! { |letter| (letter == "a" ? letter = "e" : letter.next)}
-
-	# match the string to the letters in 'aeio'
-	# and all of the consonants to the next consonant in the alphabet.
+person_name = gets.chomp
+def reverse(string)
+	string = string.downcase.split(' ').reverse
+	string = string.join(' ')
 end
+def next_letter(string)
+	string.chars.map!{|letter| letter.next}.join('')
+end
+def condition(string)
+	# create a loop that iterates over the characters
+	# for each?
+	if (# character = " " (or a !))
+		# return " "
+	elsif # character = a vowel
+		# return the next vowel respectively
+	else (# if the character is 'aa' )
+		# change it to a
+	end
+end
+puts next_letter(reverse(person_name))
 
-p secret_name(name)
 # Output => "Letu Gostu"
+
+# ---------- ATTEMPTED
+# def secret_name (name)
+# 	name = name.downcase.split(' ').reverse
+# 	name = name.join(' ').chars
+# 	name.map! { |letter| (letter == "a" ? letter = "e" : letter.next)}
+# end
+# p secret_name(name)
