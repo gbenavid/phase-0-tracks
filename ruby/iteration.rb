00f1f6a4d5b83be4bf9568bad0d7cada873dadd8
Gina Benavidez <gina.commit@gmail.com>
2017-01-12 17:56:21 -0800

    Final Commit. Learned and divulged in two new ways to iterate over an array in ruby.

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -10,22 +10,57 @@ recipe = [
 	'Dumplings', 
 	'Hash-Browns', 
 	'Curry paste', 
-	'Pizza']
+	'Pizza'
+]
+# Est. lengths for array and hash 
+	ingridient_list = ingridients.length
+	recipe_length = recipe.length
 
-ingridient_list = ingridients.length
-recipe_length = recipe.length
+	ingridient_list.times { |i| puts ingridients[i] }
+	puts ("~*~*")*7
+	recipe_length.times { |i| puts recipe[i] }
+	ingridients.each do |order, dry| 
+		puts "Mix #{order}, #{dry}"
+	end
+	puts ("~*~*")*7
+	recipe.map do |dish| recipe[3] = "Steamed Califlower"
+	end
+	p recipe
+######################
 
-ingridient_list.times { |i| puts ingridients[i] }
+count = {
+	1 => "One!", 
+	2 => "Two!", 
+	3 => "Three!", 
+	4 => "Four!", 
+	5 => "Five!"
+}
+
+array = [
+	"a", 
+	"b", 
+	"c"
+]
+digits = [1, 2, 3, 4, 5, 0, 1, 2, 3]
+				# LOOKING @ VALUES>>>>KEEPS THE REST
+p digits
+p digits.drop_while {|number| number < 3}
 puts ("~*~*")*7
-recipe_length.times { |i| puts recipe[i] }
-ingridients.each do |order, dry| 
-puts "Mix #{order}, #{dry}"
-end
-puts ("~*~*")*7
-recipe.map do |dish| recipe[3] = "Steamed Califlower"
-end
 
-p recipe
+numeric = [1, 2, 3, 4, 5, 0]
+				# LOOKING @ VALUES (likes Goolge!)
+p numeric
+p numeric.take_while {|number| number < 4}
+
+
+
+
+
+
+
+
+
+
 
 
 
