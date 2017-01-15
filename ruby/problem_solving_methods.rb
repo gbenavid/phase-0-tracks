c7e5509f32368c932cd5a7192a760a89f6fd7bfd
Gina Benavidez <gina.commit@gmail.com>
2017-01-15 14:46:13 -0800

    Psuedocoding compleated.

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -26,20 +26,28 @@ def fib (number)
 	return fib_arr
 end
 # Output => an array of numbers
-p fib(100)
+p fib(1)
 
+# Release 2
+# Input => [1, 3, 4, 12, 5, 2]
+def bubble_sort(array)
+  n = array.length
+  loop do
+    swapped = false
+# Compare each number to the next number
+# and if that number is larger than the former, print the larger into the new var called sorted
+    (n-1).times do |i|
+      if array[i] > array[i+1]
+        array[i], array[i+1] = array[i+1], array[i]
+        swapped = true
+      end
+    end
 
-
-
-
-
-
-
-
-
-
-
-
+    break if not swapped
+  end
+  array
+end
+# Output => [1, 2, 3, 4, 5, 12]
 
 
 
