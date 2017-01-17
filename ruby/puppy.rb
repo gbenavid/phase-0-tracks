906ea111a1464dad99ef4a5c35de9f3c29876d32
Gina Benavidez <gina.commit@gmail.com>
2017-01-16 19:44:28 -0800

    Puppy class pseudeocoded

---
+++ ruby/puppy.rb
@@ -0,0 +1,15 @@
+#Puppy Class
+# SPECIES ------------------------
+# Puppy
+
+# CHARACTERISTICS ----------------
+# Paw Size: varies
+# Friendly: varies
+# Tail count: 1
+# Name: varies
+# Color: varies
+
+# BEHAVIOR -----------------------
+# Jump
+# Run
+# Various tricks
