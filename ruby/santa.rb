bd16fb2eb484c90c832806aade28dd2aae7ab2ef
Gina Benavidez <gina.commit@gmail.com>
2017-01-18 09:36:24 -0800

    Looped Santa data to pair each gender with a corresponding sexual orientation. Elimiated old shoveling method.

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,19 +1,62 @@
 # Here I'm defining a class called 'Santa' along with some methods that we'll see later in the driver code.
 class Santa
+	def initialize (gender, ethnicity)
+		puts "Initializing Santa instance ..."
+		@reindeer_ranking = [
+			"Rudolph", 
+			"Dasher", 
+			"Dancer", 
+			"Prancer", 
+			"Vixen", 
+			"Comet", 
+			"Cupid", 
+			"Donner", 
+			"Blitzen"]
+		@age = 0
+		@gender = gender
+		@ethnicity = ethnicity
+		end
 	def speak
 		puts "Ho, ho, ho! Haaaappy holidays!"
 		end
-	def eat_milk_and_cookies (cookie)
+	def eat_milk_and_cookies (cookie)–
 		puts "That was a good #{cookie} cookie!"
 		end
-	def initialize
-		puts "Initializing Santa instance ..."
+	def about
+		puts "Ethnicity: #{@ethnicity}"
+		puts "Gender: #{@gender}"
+		puts "~*~*~*~*~*"		
 		end
 end
 # Driver Code
+		# Here we're creating an instance within our Santa class. 
+		# st_nick = Santa.new
+		# st_nick.speak
+		# st_nick.eat_milk_and_cookies("Vegan Chocolate Chip")
+santas = []
+example_genders = [
+	"Agender", 
+	"Female", 
+	"Bigender", 
+	"Male", 
+	"Lesbian", 
+	"Gender Fluid", 
+	"N/A"]
+example_ethnicities = [
+	"Black", 
+	"Latino", 
+	"White", 
+	"Japanese-African", 
+	"prefer not to say", 
+	"Unicorn", 
+	"N/A"]
+# santas << Santa.new("female", "Latino").about
+# santas << Santa.new("bigender", "white").about
+# santas << Santa.new("male", "Japanese").about
+# santas << Santa.new("Lesbian", "Latina").about
+# santas << Santa.new("gender fluid", "Unicorn").about
+# santas << Santa.new("N/A", "N/A").about
+example_genders.length.times do |i|
+  santas << Santa.new(example_genders[i], example_ethnicities[i]).about
+end
 
-# Here we're creating an instance within our Santa class. 
-st_nick = Santa.new
-# He can speak and eat milk and cookies through the methods we created for him:
-st_nick.speak
-st_nick.eat_milk_and_cookies("Vegan Chocolate Chip")
