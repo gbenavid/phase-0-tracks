ffb8e18df2abbf00b15f93c7f4938f398e62585c
Gina Benavidez <gina.commit@gmail.com>
2017-01-18 22:50:27 -0800

    Included Shout module inside both classes, invoked both w/ driver code

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,11 +1,24 @@
 module Shout
 	# def self.yell_angrily(words)
-	# 	words + "!!!" + '😡'
-	# end
+		# words + "!!!" + '😡'
+		# end
 	# def self.yell_happily(words)
-	# 	words + "!!!" + '😆'
-	# end
+		# words + "!!!" + '😆'
+		# end
+	def but_wait! (words)
+		puts "BUT WAIT... THERE'S MORE, " + words.upcase + "!!!"
+	end
+end
+class Thanksgiving_at_grandmas
+	include Shout
+end
+class Infomercials
+	include Shout
 end
 # DRIVER CODE
-puts Shout.yell_happily('I LOVE RUBY')
-puts Shout::yell_angrily('I WANT A BROWNIE')
+leftovers = Thanksgiving_at_grandmas.new
+leftovers.but_wait!("Turkey Legs")
+unnecessary_merchandise = Infomercials.new
+unnecessary_merchandise.but_wait!("Call now and we'll triple your order of might-tape™ ---for free")
+# puts Shout.yell_happily('I LOVE RUBY')
+# puts Shout::yell_angrily('I WANT A BROWNIE')
