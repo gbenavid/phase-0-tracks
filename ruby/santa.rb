16e51f4257ec1aab9aa2b1b0430d3704341c33c3
Gina Benavidez <gina.commit@gmail.com>
2017-01-18 18:50:39 -0800

    Added three santas to an array, each santa has a random gender & ethnicity pairing.

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -21,52 +21,57 @@ class Santa
 		@ethnicity = ethnicity
 		@celebrate_birthday = @age + 1
 		@get_mad_at = @reindeer_ranking.insert(-1, reindeer_name)
-		end
+			end
 	def speak
-		puts "Ho, ho, ho! Haaaappy holidays!"
-		end
-	def eat_milk_and_cookies (cookie)–
-		puts "That was a good #{cookie} cookie!"
-		end
-	def about
+			puts "Ho, ho, ho! Haaaappy Holidays!"
+			end
+	def eat_milk_and_cookies (cookie)
+			puts "That was a good #{cookie} cookie!"
+			end 
+	def about #PRINTING DATA
 		puts "Ethnicity: #{@ethnicity}"
 		puts "Gender: #{@gender}"
 		puts "Reindeer Ranking: #{@get_mad_at}"
 		puts "~*~*~*~*~*"		
-		end #PRINTING DATA
+		end
 end
+
 # Driver Code
 	# Creating santas by pairing them to the corresponding indices between the two arrays (genders & ethnicities)...
 		# santas = []
-		# example_genders = [
-		# 	"Agender", 
-		# 	"Female", 
-		# 	"Bigender", 
-		# 	"Male", 
-		# 	"Androgyne", 
-		# 	"Gender Fluid", 
-		# 	"N/A"]
-		# example_ethnicities = [
-		# 	"Black", 
-		# 	"Latino", 
-		# 	"White", 
-		# 	"Japanese-African", 
-		# 	"prefer not to say", 
-		# 	"Unicorn", 
-		# 	"N/A"]
+		example_genders = [
+			"Agender", 
+			"Female", 
+			"Bigender", 
+			"Male", 
+			"Androgyne", 
+			"Gender Fluid", 
+			"N/A"]
+		example_ethnicities = [
+			"Black", 
+			"Latino", 
+			"White", 
+			"Japanese-African", 
+			"prefer not to say", 
+			"Unicorn", 
+			"N/A"]
 
 		# example_genders.length.times do |i|
 		#   santas << Santa.new(example_genders[i], example_ethnicities[i], "Vixen").about
 		#  end
+
 	# Testing my work for getter & setter methods...
 		# st_nick = Santa.new("Cis Male", "White", "Vixen")
-		# puts "A #{st_nick.gender} Santa has been created, but will soon reasign her gender to 'Female'"
+		# puts "A #{st_nick.gender} Santa has been created, but will soon reasign her gender to 'Female'..."
 		# st_nick.gender = "Female"
 		# st_nick.about # after gender is reasigned
-		# puts "Our santa is #{st_nick.age} years of age, and ethinically is recognized to be: #{st_nick.ethnicity}"
-	# Creating allot of Santas using loops
-	# Assign each santa to a randomly selected gender & ethnicity pair
-	# Randomly set each Santa's age to a number between 0-140
-	# Print out the instance methods that give you access to the data
+		# puts "Our santa is #{st_nick.age} years of age, and ethinically is recognized as: #{st_nick.ethnicity}"
 
+	# Creating allot of Santas:
+santa_con = []
 
+3.times do 
+	santa_con << Santa.new(example_genders.sample, example_ethnicities.sample, "Vixen").about
+end
+
+	# Randomly set each Santa's age to a number between 0-140 >>> rand(141)
