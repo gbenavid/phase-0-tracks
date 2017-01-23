9fd85db4ba8d3b52f0b280ce8fc79b01525f7fd8
Gina Benavidez <gina.commit@gmail.com>
2017-01-23 14:30:07 -0800

    Adding a class and psedeocode for the methods within.

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,4 +1,21 @@
-# UI: Ask the user for the following information & collect the data:
+class Fact_checking
+  def valid_name(name)
+    # Name is not drakeula or tufang
+    # Returns true else false
+  end
+  def valid_age (age, dob)
+    # If their dob is not eq to year return false. If their age corresponds with their dob && they're <= 120 return true
+  end
+  def likes_garlic? (preference)
+    # returns true or false based off of a value entered
+    # value.downcase == 'yes' ? true : false
+  end
+  def healthcare(enrollment)
+    # returns true or false based off of a value entered
+    # value.downcase == 'yes' ? true : false 
+  end
+end
+# UI: Ask the user for the following information
 puts "What's your name?"
 puts "How old are you?"
 puts "What year were you born?"
