d0513076d80592e959099070755cb294011773c8
Gina Benavidez <gina.commit@gmail.com>
2017-01-25 10:41:46 -0800

    Finished function baseline. Working on bugs that declare 'undefined'

--- js/explore.js
+++ js/explore.js
@@ -1,7 +1,7 @@
 // Pass your reversed function a parameter(string)
 function reversed (str){
 	// Set an empty string that we will use latter on
-  var reverse = '';
+  var reverse = "";
   // Loop through the string backwards
   for (var i = str.length; i > -1; i--){
   // Keep adding your letter to the empty string
@@ -9,28 +9,42 @@ function reversed (str){
   }
   console.log(reverse);
 }
-// Ignite your function like so:
-reversed("Kitty")
+// Store results of driver code:
+var storage = reversed("LOLA");
 
-// How to count down:
-for (var i = 10; i >= 0; i-=1) {
-	console.log([i]);
+
+var  num_1 = 1;
+var num_2 = 1;
+
+if (num_1 === num_2) {
+	console.log(storage);
+} else {
+	console.log("Opps! Something went wrong...");
 }
 
-// How to loop through a string
-var str = "star";
-for (var i = 0; i < str.length; i++) {
-	console.log(str[i]);
-}
 
-// How to add two numbers
-function add(x, y) {
-  return x + y;
-}
-console.log(add(5,3));
 
-// How to say hello
-function sayHello() {
-  console.log("Hello!");
-}
-sayHello();
+// ------------------------------------------------
+
+// // How to count down:
+// for (var i = 10; i >= 0; i-=1) {
+// 	console.log(i);
+// }
+
+// // How to loop through a string
+// var str = "star";
+// for (var i = 0; i < str.length; i++) {
+// 	console.log(str[i]);
+// }
+
+// // How to add two numbers
+// function add(x, y) {
+//   return x + y;
+// }
+// console.log(add(5,3));
+
+// // How to say hello
+// function sayHello() {
+//   console.log("Hello!");
+// }
+// sayHello();
