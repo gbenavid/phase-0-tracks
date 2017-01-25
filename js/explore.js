daee79fe233bf2b8fb889b8b48f3a3e08e4cfeae
Gina Benavidez <gina.commit@gmail.com>
2017-01-25 10:13:27 -0800

    Added some notes to self

--- js/explore.js
+++ js/explore.js
@@ -4,5 +4,28 @@ function reversed (str){
   reverse += str[i];
   }
   console.log(reverse);
-  
 }
+reversed("Kitty")
+
+// How to count down:
+for (var i = 10; i >= 0; i-=1) {
+	console.log([i]);
+}
+
+// How to loop through a string
+var str = "star";
+for (var i = 0; i < str.length; i++) {
+	console.log(str[i]);
+}
+
+// How to add two numbers
+function add(x, y) {
+  return x + y;
+}
+console.log(add(5,3));
+
+// How to say hello
+function sayHello() {
+  console.log("Hello!");
+}
+sayHello();
