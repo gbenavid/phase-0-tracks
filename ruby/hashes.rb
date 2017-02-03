9ef1a180b9ed0bdafe066d50fa144e403ab90b89
Gina Benavidez <gina.commit@gmail.com>
2017-02-03 11:11:11 -0800

    In the middle of revising

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,20 +1,20 @@
 # ----- The Program -----
 # Note: Use symbols
-p "Please type carefully when entering the following information about your client."
+puts "Please type carefully when entering the following information about your client."
 # Promt the user for all of this information.
   # client: 
   # name, 
   # age, 
   # number of children, and
   # decor theme
-p "Name of client:"
+puts "Name of client:"
 client_name = gets.chomp
-p "Age:"
+puts "Age:"
 age = gets.chomp
-p "How many kids does your client have? (enter 0 if none)"
+puts "How many kids does your client have? (enter 0 if none)"
 # Convert any user input to the appropriate data type. 
 kids = gets.chomp.to_i
-p "The decor is a custom theme (true/false)?"
+puts "The decor is a custom theme (true/false)?"
 custom_theme = gets.chomp
 
 # -----client data in an array with key values-----
@@ -24,19 +24,19 @@ client_data = {
   :kids => kids,
   :custom_theme => custom_theme}
 # Print the hash back out to the screen when the designer has answered all of the questions.
-p client_data
-p "^ ^ ^ ^ ^ ^ Does all that information look right to you?"
-p "If all the information is correct type 'none', otherwise press enter"
+puts client_data
+puts "Does all that information look right to you?"
+puts "If all the information is correct type 'none', otherwise press enter"
 response = gets.chomp
 if response == 'none'
-  p "Okay great! Here are your final results:"
+  puts "Okay great! Here are your final results:"
 elsif     
-  p "Okay, what feild needs a correction (e.g client_name)?"
+  puts "Okay, what feild needs a correction (e.g client_name)?"
   correct_sym = gets.chomp.to_sym
-  p "What would you like it to say instead?"
+  puts "What would you like it to say instead?"
   correct_value = gets.chomp
   #update corresonding key
   client_data[correct_sym] = correct_value
 end
 # Print the latest version of the hash, and exit the program.
-p client_data
+puts client_data
