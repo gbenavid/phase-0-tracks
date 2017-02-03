7e70e25f5867cb18399814e6a5af7e20ac5d596a
Gina Benavidez <gina.commit@gmail.com>
2017-02-03 09:49:53 -0800

    culprit 7 was caught using four partners in crime (slice, reverse, chomp, and chop)

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -1,26 +1,34 @@
+puts "~*"*6 + " culprit 1 was caught?"
 "iNvEsTiGaTiOn".swapcase 
 puts "iNvEsTiGaTiOn".swapcase.eql? "InVeStIgAtIoN"
 
+puts "~*"*6 + " culprit 2 was caught?"
 puts "zom".gsub(/[o]/, "oo").eql? "zoom"
 puts "zom".insert(1, "o").eql? "zoom"
 puts "zom".replace("zoom").eql? "zoom"
 
+puts "~*"*6 + " culprit 3 was caught?"
 puts "enhance".center(15).eql? "    enhance    "
 
+puts "~*"*6 + " culprit 4 was caught?"
 puts "Stop! You’re under arrest!".upcase.eql? "STOP! YOU’RE UNDER ARREST!"
 
+puts "~*"*6 + " culprit 5 was caught?"
 puts "the usual".concat(" suspects").eql? "the usual suspects"
 puts ("the usual" << " suspects").eql? "the usual suspects"
 puts "the usual".insert(-1, " suspects").eql? "the usual suspects"
+puts " suspects".reverse.concat("lausu eht").reverse.eql? "the usual suspects"
 
-" suspects".reverse.concat("lausu eht").reverse
-# => "the usual suspects"
+puts "~*"*6 + " culprit 6 was caught?"
 
-"The case of the disappearing last letter".chop 
-# => "The case of the disappearing last lette"
+puts "The case of the disappearing last letter".chop.eql? "The case of the disappearing last lette"
+puts "The case of the disappearing last letter".chomp("r").eql? "The case of the disappearing last lette"
+puts "The case of the disappearing last letter".slice(0..38).eql? "The case of the disappearing last lette"
 
-"The mystery of the missing first letter".reverse.chop.reverse
-# => "he mystery of the missing first letter"
+puts "~*"*6 + " culprit 7 was caught?"
+puts "The mystery of the missing first letter".reverse.chop.reverse.eql? "he mystery of the missing first letter"
+puts "The mystery of the missing first letter".slice(1..39).eql? "he mystery of the missing first letter"
+
 
 # "Elementary,    my   dear        Watson!".<???>
 # => "Elementary, my dear Watson!"
