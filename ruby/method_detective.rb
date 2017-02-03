a0a29d7ec89ed1114aaf177c305a3eba5b235045
gbenavid <gbenavid@my.hpu.edu>
2017-02-03 10:12:42 -0800

    Update method_detective.rb

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -1,39 +1,40 @@
-# Replace in the "<???>" with the appropriate method (and arguments, if any).
-# Uncomment the calls to catch these methods red-handed.
-
-# When there's more than one suspect who could have committed the crime, add additional calls to prove it.
-
+puts "~*"*6 + " culprit 1 was caught?"
 "iNvEsTiGaTiOn".swapcase 
-# => “InVeStIgAtIoN”
+puts "iNvEsTiGaTiOn".swapcase.eql? "InVeStIgAtIoN"
 
-# "zom".tr('o', 'oo')
-# => “zoom”
+puts "~*"*6 + " culprit 2 was caught?"
+puts "zom".gsub(/[o]/, "oo").eql? "zoom"
+puts "zom".insert(1, "o").eql? "zoom"
+puts "zom".replace("zoom").eql? "zoom"
 
-"enhance".center(20) 
-# => "    enhance    "
+puts "~*"*6 + " culprit 3 was caught?"
+puts "enhance".center(15).eql? "    enhance    "
 
-"Stop! You’re under arrest!".upcase 
-# => "STOP! YOU’RE UNDER ARREST!"
+puts "~*"*6 + " culprit 4 was caught?"
+puts "Stop! You’re under arrest!".upcase.eql? "STOP! YOU’RE UNDER ARREST!"
 
-"the usual".concat(" suspects")
-"the usual" << " suspects" 
-#=> "the usual suspects"
+puts "~*"*6 + " culprit 5 was caught?"
+puts "the usual".concat(" suspects").eql? "the usual suspects"
+puts ("the usual" << " suspects").eql? "the usual suspects"
+puts "the usual".insert(-1, " suspects").eql? "the usual suspects"
+puts " suspects".reverse.concat("lausu eht").reverse.eql? "the usual suspects"
 
-" suspects".reverse.concat("lausu eht").reverse
-# => "the usual suspects"
+puts "~*"*6 + " culprit 6 was caught?"
 
-"The case of the disappearing last letter".chop 
-# => "The case of the disappearing last lette"
+puts "The case of the disappearing last letter".chop.eql? "The case of the disappearing last lette"
+puts "The case of the disappearing last letter".chomp("r").eql? "The case of the disappearing last lette"
+puts "The case of the disappearing last letter".slice(0..38).eql? "The case of the disappearing last lette"
 
-"The mystery of the missing first letter".reverse.chop.reverse
-# => "he mystery of the missing first letter"
+puts "~*"*6 + " culprit 7 was caught?"
+puts "The mystery of the missing first letter".reverse.chop.reverse.eql? "he mystery of the missing first letter"
+puts "The mystery of the missing first letter".slice(1..39).eql? "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
-# => "Elementary, my dear Watson!"
+puts "~*"*6 + " culprit 8 was caught?"
+puts "Elementary,    my   dear        Watson!".squeeze(" ").eql? "Elementary, my dear Watson!"
 
-"z".ord
-# => 122 
+puts "~*"*6 + " culprit 9 was caught?"
+puts "z".ord.eql? 122 
 # 122 is the integer ordinal of z.
 
-"How many times does the letter 'a' appear in this string?".count "a"
-# => 4
+puts "~*"*6 + " culprit 10 was caught?"
+puts "How many times does the letter 'a' appear in this string?".count("a").eql? 4
