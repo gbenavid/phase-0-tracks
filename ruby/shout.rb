5b82d9003391e79a8ee86e197cf5a26d1696cc9c
gbenavid <gbenavid@my.hpu.edu>
2017-02-10 18:51:53 -0800

    Update shout.rb

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,16 +1,47 @@
 module Shout
-	def but_wait! (words)
-		puts "BUT WAIT... THERE'S MORE, " + words.upcase + "!!!"
-	end
+  def im_yelling(words)
+    puts words.upcase + "!!!"
+  end
 end
-# Classes (Incl. Shout Module)
+class Angry
+  include Shout
+end
+class Happy
+  include Shout
+end
+mad = Angry.new
+mad.im_yelling("I love Ruby(Tuesday's)")
+happy = Happy.new
+happy.im_yelling("I said I'm fine")
+
+module Flight
+  def take_off(altitude)
+    puts "Taking off and ascending until reaching #{altitude} ..."
+  end
+end
+class Bird
+  include Flight
+end
+class Plane
+  include Flight
+end
+# DRIVER CODE
+bird = Bird.new
+bird.take_off(800)
+plane = Plane.new
+plane.take_off(30000)
+
+module Shout
+  def but_wait! (words)
+    puts "BUT WAIT... THERE'S MORE, " + words.upcase + "!!!"
+  end
+end
 class Thanksgiving_at_grandmas
-	include Shout
+  include Shout
 end
 class Infomercials
-	include Shout
+  include Shout
 end
-
 # DRIVER CODE
 leftovers = Thanksgiving_at_grandmas.new
 leftovers.but_wait!("Turkey Legs 🍠🥕🍗")
