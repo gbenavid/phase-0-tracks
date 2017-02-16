ac81e88f25541c94653dda44bb4a29ebe0544809
gbenavid <gbenavid@my.hpu.edu>
2017-02-16 14:05:01 -0800

    Update algo.js

--- js/algo.js
+++ js/algo.js
@@ -25,8 +25,31 @@ function compareObjs(obj, objTwo){
   }
   return false;
 }
-//TESTING
+//TESTING COMPAREOBJS();
 console.log(compareObjs(
   {name:"Tu", age:28}, 
   {name:"To", age:29})
 );
+
+//TESTING FIND LONGEST WORD FUNCTION WITH A LOOP
+//Get a random number for your generateRandomNumber function
+function generateRandomNumber (){
+  return Math.floor((Math.random() * 10) + 1);
+}
+//Get a random array for your find longestWord function
+function generateTests(number){
+  array = [];
+  word = "";
+  characters = 'abcdefghijklmnopqrstuvwxyz';
+  for (i = 0; i <= number; i++){
+    word += characters.charAt(i);
+    array.push(word);
+  }
+  return array;
+}
+ test = function testingRandomStrings (){
+  for(var i=0; i < 10; i++){
+    console.log(generateTests(generateRandomNumber()));
+  }
+};
+test();
