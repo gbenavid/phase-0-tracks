530224f520d85dc723c3e075d3bfa54b4a86ba77
gbenavid <gbenavid@my.hpu.edu>
2017-02-16 11:12:54 -0800

    Update algo.js

--- js/algo.js
+++ js/algo.js
@@ -19,51 +19,14 @@ var filterLongWords = function(array){
 }
 // console.log(findLongestWord(["Grandma's Cookies", "All the love you need", "Take me home, when september ends..."]));
 // console.log(filterLongWords(["Grandma's Cookies", "All the love you need", "Take me home, when september ends...", "your no good, your no good, your no goooodd."]));
-
-function compareObjs(array){
-	var obj_1 = array[0];
-	var obj_2 = array[1];
-  if (Object.keys(obj_1.keys) == Object.keys(obj_2.keys)) {
-  	return true;
-  }else{
-  	return false;
+function compareObjs(obj, objTwo){
+  for (var key in obj){
+    if (obj[key] === objTwo[key]) return true;
   }
+  return false;
 }
-
-
 //TESTING
-console.log(compareObjs[
-	{name: "Steven", age: 54},
-	{name: "Tamir", age: 54}]);
-// >> true
-
-
-
-
-//NOTES:
-// var an_obj = { 100: 'a', 2: 'b', 7: 'c' };
-//console.log(Object.keys(an_obj));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+console.log(compareObjs(
+  {name:"Tu", age:28}, 
+  {name:"To", age:29})
+);
