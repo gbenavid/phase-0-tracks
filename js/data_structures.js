718d033b55331a5e4543e1a451ace350adb5e1d9
gbenavid <gbenavid@my.hpu.edu>
2017-02-15 18:35:30 -0800

    Update data_structures.js

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,64 +1,28 @@
-// We need to assign colors to horses. 
-// Add code that will use your two arrays to create an object.
-
-// The keys of your object should be horse names, 
-// and the values should be colors. 
-// Your solution should be something that would work for any number of colors/horses, as long as the two arrays are the same length. 
 var names = ["Edward", "Chris", "Christina", "Tabitha"];
-var colors = ["blue", "orange", "green", "yellow"];
-colors.push("black");
+var colors = ["Blue", "Orange", "Green", "Yellow"];
+colors.push("Black");
 names.push("Henry");
 
-// var dog = {name: null, 'age': 3, isGoodDog: true};
-
-function color_my_horse(horses, color) {
-	var colorful_horses = {};
-	for (var h = 0; h < color.length; h++){
-		 //>>color[i]		
-var colors = ["blue", "orange", "green", "yellow"];
-var names = ["Edward", "Chris", "Christina", "Tabitha"];
-colors.push("black");
-names.push("Henry");
-thing = {};
-for(var i = 0; i < colors.length; i++){
-  thing[names[i]] = colors[i];
+function color_my_horses(horses, colors){
+	colorful_horses = {}
+	for (i = 0; i < horses.length; i++){
+		colorful_horses[horses[i]]=colors[i];
+	}
+	return colorful_horses;
 }
- console.log(thing)
- //release 2:
- function Car(model, year, isFast) {
-  
-  console.log("Our new car:", this);
-  
-this.model = model;
-this.year = year;
-this.isFast = isFast;
-this.race = function() {
-  console.log("ZOOM!"); 
-    
-  };
-  
-  console.log("Ready, Set, Go!.....");
+console.log(color_my_horses(names, colors))
+
+// Creating a car using a constructor function
+function Car (make, year, isFast) {
+  this.make = make;
+     this.year = year;
+     this.isFast = isFast;
+     this.zoom = function() {
+       console.log("ZOOOM!");
+     };
 }
-console.log("Let's build a car ...");
-var anotherCar = new Car("Toyota Camry", 2012, true);
-console.log(anotherCar);
-console.log("Our car can race:");
-anotherCar.race();
-console.log("-~-~-");
-console.log("Let's build another car ...");
-var yetAnotherCar = new Car("Prius C class", 2016, false);
-console.log(yetAnotherCar.model);
-console.log("This new car can race, too:");
-yetAnotherCar.race();
-console.log("-~-~-")
-console.log("Let's build another car ...");
-var ourThirdCar = new Car("Tesla", 1874, false);
-console.log(ourThirdCar.model);
-console.log("This new car cannot race....");
-console.log("-~-~-")
-
-
-
-
-
-
+police = new Car('Honda', 2012, true);
+console.log('This ' + police.make + ' is a police car that goes... ');
+police.zoom();
+taxi = new Car ('Mazda', 2010, false)
+console.log('This taxi driver drives a ' + taxi.make + ' and it is not very fast...');
