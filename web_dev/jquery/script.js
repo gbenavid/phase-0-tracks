a5afe8d66bc47822d76cc0d1a6247b4c53da465d
Gina Benavidez <gina.commit@gmail.com>
2017-02-28 14:09:58 -0800

    added an event driven JS listner in script

--- web_dev/jquery/script.js
+++ web_dev/jquery/script.js
@@ -1,2 +1,10 @@
 console.log('The script is running');
-var test = 
+test = document.getElementById("navigation-pane");
+
+test.addEventListener("mouseover", function( event ) {   
+    event.target.style.color = 'silver';
+    setTimeout(function() {
+      event.target.style.color = "";
+    }, 500);
+  }, false);
+  
