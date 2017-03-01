aa92ece6b2534ffd72da4b4ff431a33f3921fd4b
Gina Benavidez <gina.commit@gmail.com>
2017-03-01 07:19:34 -0800

    additional notes added to app.rb

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -2,15 +2,15 @@
 require 'sinatra'
 require 'sqlite3'
 
-set :public_folder, File.dirname(__FILE__) + '/static'
+# set :public_folder, File.dirname(__FILE__) + '/static'
 
 db = SQLite3::Database.new("students.db") # creting a new db w/ the student info
 db.results_as_hash = true #recieving our database results as a hash
 
 # show students on the home page
 get '/' do
-  @students = db.execute("SELECT * FROM students")
-  erb :home
+  @students = db.execute("SELECT * FROM students") # getting our students, the @ sign lets us set a var that is accessible in our template
+  erb :home #This is how we return the home erb file, makeing a template to return. Erb is a method, and it's parameter is home
 end
 
 get '/students/new' do
@@ -19,7 +19,7 @@ end
 
 # create new students via
 # a form
-post '/students' do
+post '/students' do #We use POST instead of GET
   db.execute("INSERT INTO students (name, campus, age) VALUES (?,?,?)", [params['name'], params['campus'], params['age'].to_i])
   redirect '/'
 end
